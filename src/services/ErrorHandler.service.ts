03ae87177b512f58b820d1e3c4a0b00ff067eb9e
Pourya Akraminayeri <pak@trackman.com>
2023-02-22 14:30:20 +0100

    Improve the error handling function

--- src/services/ErrorHandler.service.ts
+++ src/services/ErrorHandler.service.ts
@@ -1,20 +1,21 @@
 import { ResponseError } from "@/types/ResponseError";
 
-export function handleError(err: unknown) {
+export function handleError(err: unknown): void {
     if (err instanceof ResponseError) {
         switch (err.response.status) {
             case 401:
-                alert("The request lack valid authentication credentials")
+                alert("The request lacks valid authentication credentials.");
                 break;
             case 404:
-                alert("Data not found!")
+                alert("Data not found!");
                 break;
             case 500:
-                alert("Internal server error")
+                alert("Internal server error.");
                 break;
             default:
-                throw new Error('Unhandled fetch response', { cause: err })
+                throw new Error('Unhandled fetch response');
         }
+    } else {
+        throw new Error('Unknown fetch error');
     }
-    throw new Error('Unknown fetch error', { cause: err })
 }
